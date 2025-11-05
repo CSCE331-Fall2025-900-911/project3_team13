3c0f9651a083994723f1394503bbb93b24bf7811
root <root@JaredsTower.>
2025-11-04 23:33:15 -0600

    Fixed deployment

--- cashier/src/App.tsx
+++ cashier/src/App.tsx
@@ -1,28 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  TextField,
+  // Button,
+  // Dialog,
+  // DialogTitle,
+  // DialogContent,
+  // DialogActions,
+  // TextField,
   Tabs,
   Tab,
-  Radio,
-  RadioGroup,
-  FormControlLabel,
-  FormControl,
-  FormLabel
+  // Radio,
+  // RadioGroup,
+  // FormControlLabel,
+  // FormControl,
+  // FormLabel
 } from '@mui/material';
 import { LogoutButton } from './components/LogoutButton.tsx';
 
 function App() {
   const [tabValue, setTabValue] = useState<'menu' | 'library' | 'orders'>('menu');
-  const [open, setOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState('option1');
+  // const [open, setOpen] = useState(false);
+  // const [selectedOption, setSelectedOption] = useState('option1');
 
   return (
     <>
@@ -35,7 +33,7 @@ function App() {
       </header>
       <div className="tabPanel">
         {/* Main content goes here */}
-        <Tabs value={tabValue} onChange={(e, newValue) => setTabValue(newValue)}>
+        <Tabs value={tabValue} onChange={(__, newValue) => setTabValue(newValue)}>
           <Tab label="Main Menu" value="menu" />
           <Tab label="Library" value="library" />
           <Tab label="Orders" value="orders" />
