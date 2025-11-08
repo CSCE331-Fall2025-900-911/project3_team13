ca3585f49e26f07daf641bcb4203afe3888856c2
Jared White <jaredwhite4@tamu.edu>
2025-11-08 11:26:50 -0600

    Included missing dummy data

---
+++ cashier/src/data/dummySeries.ts
@@ -0,0 +1,43 @@
+// Type definition for our menu items
+export type MenuItem = {
+    id: number;
+    name: string;
+    icon: string;
+};
+
+// Import the React logo (you can change this to other images later)
+import reactLogo from '../assets/react.svg';
+
+// Dummy data array
+export const dummySeries: MenuItem[] = [
+    {
+        id: 1,
+        name: "Milk Tea",
+        icon: reactLogo
+    },
+    {
+        id: 2,
+        name: "Unicorn Tea",
+        icon: reactLogo
+    },
+    {
+        id: 3,
+        name: "Milk Tea",
+        icon: reactLogo
+    },
+    {
+        id: 4,
+        name: "Unicorn Tea",
+        icon: reactLogo
+    },
+    {
+        id: 5,
+        name: "Milk Tea",
+        icon: reactLogo
+    },
+    {
+        id: 6,
+        name: "Unicorn Tea",
+        icon: reactLogo
+    }
+];
