04e937790d789f3cfac3de05aefddf9d1ea70423
obinnanprogrammed <obinnanwakwue10@gmail.com>
2025-11-10 11:15:08 -0600

    fixed ID generation issue

--- backend-cashier/routes/newOrderRoute.js
+++ backend-cashier/routes/newOrderRoute.js
@@ -6,9 +6,7 @@ const pool = require('../db/pool');
 router.post('/', async (req, resp) => {
     try {
         const client = await pool.connect();
-        const lastOrderId = await client.query('SELECT MAX(id) FROM orders;');
-        const res = await client.query('INSERT INTO orders (id, status, timestamp) VALUES ($1, $2, $3) RETURNING id;', [
-            (lastOrderId.rows[0].max || 0) + 1,
+        const res = await client.query('INSERT INTO orders (status, timestamp) VALUES ($1, $2) RETURNING id;', [
             false,
             new Date().toISOString()
         ]);
