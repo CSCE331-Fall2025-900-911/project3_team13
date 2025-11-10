a6a5344f734b449ddf930189b47d5ab495258c86
IanNguyenIIW <ianjamesijh101@gmail.com>
2025-11-10 14:50:17 -0600

    addmoficied menu item route done i think

--- backend-cashier/routes/addModifiedMenuItemRoute.js
+++ backend-cashier/routes/addModifiedMenuItemRoute.js
@@ -6,7 +6,7 @@ const pool = require('../db/pool');
 router.post('/', async (req, res) => {
     // requests to this route should contain order ID, menu item ID, and a list of modifications
     // ideally the modifications should be of the form [{itemName: "item name", quantity: x}, ...]
-    const { orderId, menuItemId, modifications } = req.body;
+    const { orderId, menuItemId, sugar, ice, size, shots, notes } = req.body;
     
     try {
         const client = await pool.connect();
@@ -19,21 +19,17 @@ router.post('/', async (req, res) => {
         ]);
 
         // how is quantity represented?
-        for(const mod of modifications) {
-            let { itemName, quantity } = mod;
-            console.log(itemName, quantity);
-            const invItemRes = await client.query('SELECT id from inventory WHERE name = $1;', [itemName]);
-            console.log(invItemRes.rows.length);
-            if(invItemRes.rows.length > 0) {
-                const invItemId = invItemRes.rows[0].id;
-
-                await client.query('INSERT INTO item_editing_table (comboid, inventoryid, quantity) VALUES ($1, $2, $3);', [
-                    menuItemOrderId,
-                    invItemId,
-                    quantity
-                ]);
-            }
-        }
+        
+        await client.query('INSERT INTO item_editing_table (comboid, sugar, ice, size, shots, notes) VALUES ($1, $2, $3, $4, $5, $6);', [
+            menuItemOrderId,
+            sugar,
+            ice,
+            size,
+            shots,
+            notes
+        ]);
+        
+        
         client.release();
         res.status(201).json({ message: 'Item added to order successfully' });
     } catch(err) {
