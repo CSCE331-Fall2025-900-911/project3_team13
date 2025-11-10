bf56209d6c30a468e6181e0be5c728be30497ba2
obinnanprogrammed <obinnanwakwue10@gmail.com>
2025-11-10 12:21:19 -0600

    fixed CORS error

--- backend-cashier/index.js
+++ backend-cashier/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const pool = require('./db/pool');
 const dotenv = require('dotenv');
-
-
+const cors = require('cors');
 
 dotenv.config();
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+const corsConfig = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
 
+app.use(express.json());
+app.use(cors(corsConfig));
 
 // Routes
 app.use('/api/new-order', require('./routes/newOrderRoute'));
