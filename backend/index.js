0c74068d3feb9ab062ea010a9e6db01c438ca41a
IanNguyenIIW <ianjamesijh101@gmail.com>
2025-12-07 01:21:17 -0600

    merge index.js backend add customer call

--- backend/index.js
+++ backend/index.js
@@ -52,13 +52,10 @@ app.use('/auth', require('./routes/authRoute'));
 app.use('/api/inventory', require('./routes/managerInventoryRoutes'));
 app.use('/api/employees', require('./routes/managerEmployeeRoutes'));
 app.use('/api/manager-analytics', require('./routes/managerAnalyticsRoutes'));
-<<<<<<< HEAD
 app.use('/api/customers', require('./routes/getCustomerPoints'));
 app.use('/api/customers', require('./routes/redeemCustomerPoints'));
 
-=======
 app.use('/api/weather', require('./routes/getWeatherData'));
->>>>>>> 558914b01cf5e6101ef5e2f1b5d46213285e4c30
 
 //manager update
 app.use('/api/update-menu-item', require('./routes/updateMenuItem'));
