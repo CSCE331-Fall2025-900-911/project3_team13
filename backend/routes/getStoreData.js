906b9dc80424e8c509ec56a883643277c3cc1f47
obinnanprogrammed <obinnanwakwue10@gmail.com>
2025-12-09 11:49:55 -0600

    added awaits to fix server issues

--- backend/routes/getStoreData.js
+++ backend/routes/getStoreData.js
@@ -4,9 +4,9 @@ const pool = require('../db/pool');
 
 router.get('/', async (req, res) => {
     try {
-        const inventoryQuery = pool.query("SELECT id, name, quantity FROM inventory ORDER BY id;");
-        const menuQuery = pool.query("SELECT id, name, category, price FROM menu_items ORDER BY id;");
-        const employeeQuery = pool.query("SELECT id, name, username, permissions FROM employees ORDER BY id;");
+        const inventoryQuery = await pool.query("SELECT id, name, quantity FROM inventory ORDER BY id;");
+        const menuQuery = await pool.query("SELECT id, name, category, price FROM menu_items ORDER BY id;");
+        const employeeQuery = await pool.query("SELECT id, name, username, permissions FROM employees ORDER BY id;");
 
         const [inventoryResult, menuResult, employeeResult] = await Promise.all([
             inventoryQuery, menuQuery, employeeQuery
